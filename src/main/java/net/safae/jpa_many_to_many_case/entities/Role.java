bf5ac1cc2bd9a340ff564d815aebe3f52bdc26ef
LENOVO <s.eraji@edu.umi.ac.ma>
2025-05-14 21:28:47 +0100

    Reste du code

--- src/main/java/net/safae/jpa_many_to_many_case/entities/Role.java
+++ src/main/java/net/safae/jpa_many_to_many_case/entities/Role.java
@@ -1,9 +1,11 @@
 package net.safae.jpa_many_to_many_case.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 
@@ -12,9 +14,13 @@ import java.util.List;
 public class Role {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(length = 20, unique = true)
     private String roleName;
+    @Column(name = "DESCRIPTION")
     private String desc;
     @ManyToMany(fetch = FetchType.LAZY)
     //@JoinTable(name = "USERS_ROLES")
+    @ToString.Exclude
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private List<User> users;
 }
--- src/main/java/net/safae/jpa_many_to_many_case/entities/User.java
+++ src/main/java/net/safae/jpa_many_to_many_case/entities/User.java
@@ -1,6 +1,7 @@
 package net.safae.jpa_many_to_many_case.entities;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,7 +18,9 @@ import java.util.List;
 public class User {
     @Id
     private String userId;
+    @Column(name = "USER_NAME", unique = true, length = 20)
     private String username;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     @ManyToMany(mappedBy = "users", fetch = FetchType.EAGER)
     private List<Role> roles = new ArrayList<>(); // Pour éviter "NullPointerException"
